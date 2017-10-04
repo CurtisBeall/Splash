78dfd62e08af614842581810c5ea5174742ba1d6
Curtis Beall <curtisbeall@outlook.com>
2017-10-03 22:35:51 -0700

    Website is Complete
    HTML/CSS/JavaScript is complete.
    Added CSS for Work Example hover on Work page.
    Removed Home page/ Add About page JavaScript.
    Added a Contact Us section to About page.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-
-//Drop Down Menu
+// Drop Down Menu Function
 (function() {
 	const toggle = document.getElementById("c-hamburger");
 	const hide = document.getElementById("nav-bar");
@@ -11,88 +10,87 @@
 	});
 })();
 
+//Setup About Page Before Load Function
+window.onbeforeunload = () => {
+	
+	const about = 'file:///C:/Users/Curtis/Documents/Git%20Repos/Splash/about.html';
+	
+     if (window.location.href === about) {
+		window.scrollTo(0,0);
+	 }
+};
+
+//Setup About Page Onload Function
+window.onload = () => {
+	
+	const about = 'file:///C:/Users/Curtis/Documents/Git%20Repos/Splash/about.html';
+	
+     if (window.location.href === about) {
+		window.scrollBy(0,1);
+	 }
+};
 
 
+//About Page Text Scroll Var
 
+	let scrollToggle = 1;
+	let spanlife = -52;
+	let lastScrollY = 0;
 
-//// Global Variables
-//const headerbarHeight = document.getElementById('headerbar').clientHeight;
-//const homepage = 'file:///C:/Users/Curtis/Documents/Git%20Repos/Splash/index.html'
-//
-//// Remove header color on home page
-//window.onload = () => {
-//	
-//	console.log(document.documentElement.clientWidth);
-//
-//    const headerBar = document.getElementById("headerbar");
-//
-//    if (scrollY > 750 && window.location.href == homepage) {
-//        headerBar.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-//    } else if (scrollY < headerbarHeight * 1.3 && window.location.href != homepage) {
-//        headerBar.style.backgroundColor = 'rgb(0, 0, 0)';
-//    } else if (scrollY > headerbarHeight * 1.3 && window.location.href != homepage) {
-//        headerBar.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-//    } else {
-//        headerBar.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-//    }
-//
-//}
-//// window.onload = () => {
-////     const add_class = document.getElementById("what_we_do_text").children;
-////     for (let i = 0; i < add_class.length; i++) {
-////             add_class[i].classList += (' what_we_do_text_scroll_in');
-////     }
-////     window.scrollBy(0,1);
-//// };
-//
-//// Onscroll Functions
-//window.onscroll = () => {
-//
-//    // Creates header backgroundcolor
-//	
-//    const headerBar = document.getElementById("headerbar");
-//
-//    if (scrollY > 750 && window.location.href == homepage) {
-//        headerBar.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-//    } else if (scrollY < headerbarHeight * 1.3 && window.location.href != homepage) {
-//        headerBar.style.backgroundColor = 'rgb(0, 0, 0)';
-//    } else if (scrollY > headerbarHeight * 1.3 && window.location.href != homepage) {
-//        headerBar.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-//    } else {
-//        headerBar.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-//    }
-//
-//
-//    // Makes "billboard" and "What We Do" text scroll out/in on home page
-//    if (window.location.href === 'file:///C:/Users/Curtis/Documents/Git%20Repos/Splash/index.html') {
-//
-//        const screenHeight = document.documentElement.clientHeight - headerbarHeight;
-//        const billboard = document.getElementById("billboard_text");
-//        let billboard_pos = 0;
-//        const what_we_do = document.getElementById("what_we_do_text");
-//        let what_we_do_pos = screenHeight * 2;
-//
-//        document.getElementById("what_we_do_text").style.position = "absolute";
-//
-//        // Makes "billboard" text scroll out/in
-//        if (scrollY >= screenHeight) {
-//            billboard.style.left = 0;
-//            console.log(billboard.style.left);
-//        } else {
-//            billboard_pos = -scrollY;
-//            billboard.style.left = billboard_pos + 'px';
-//            console.log(billboard.style.left);
-//        }
-//
-//
-//        // Makes "What We Do" text scroll out/in
-//        if (scrollY * 2 >= screenHeight * 2) {
-//            what_we_do.style.left = 0;
-//        } else {
-//            what_we_do_pos += -scrollY * 2;
-//            what_we_do.style.left = what_we_do_pos + 'px';
-//        }
-//
-//    }
-//
-//};
+//About Page Text Scroll Function
+window.onscroll = () => {
+	if (window.innerWidth > 626) {
+	const staff_textR = document.getElementsByClassName("staff_textbg align-r");
+	const staff_textL = document.getElementsByClassName("staff_textbg align-l");
+	
+	let newScroll = (scrollY * 100 / document.documentElement.scrollHeight);
+	let lastScroll = (lastScrollY * 100 / document.documentElement.scrollHeight);
+	let scrollYPercent = (newScroll - lastScroll)*1.25;
+	let span = spanlife;
+	let spanL = spanlife+50;
+
+	for (var i = 0; i < staff_textR.length; i++) {
+		
+		if(span < -53 ) {
+			
+			(scrollToggle === 1 ) ?  scrollToggle = 0: scrollToggle = 1;
+			
+		} if(span > 2) {
+			
+			(scrollToggle === 0 ) ?  scrollToggle = 1: scrollToggle = 0;
+			
+		} if(scrollToggle === 0) {
+			
+			(lastScroll < newScroll) ?  span -= scrollYPercent : span -= scrollYPercent;
+			(lastScroll < newScroll) ?  lastScrollY = scrollY: lastScrollY = scrollY;
+			spanlife = span;	
+			staff_textR[i].style.transform = "translateY(-50%) translateX(" + span + "%)";
+
+		} if(scrollToggle === 1) {
+			
+			(lastScroll > newScroll) ?  span += scrollYPercent : span += scrollYPercent;
+			(lastScroll > newScroll) ?  lastScrollY = scrollY: lastScrollY = scrollY;
+			spanlife = span;
+			staff_textR[i].style.transform = "translateY(-50%) translateX(" + span + "%)";
+		}
+
+	}
+	
+
+	for (var i = 0; i < staff_textL.length; i++) {
+
+	if(scrollToggle === 0) {
+
+			(lastScroll < newScroll) ?  spanL -= scrollYPercent : spanL -= scrollYPercent;
+			staff_textL[i].style.transform = "translateY(-50%) translateX(" + spanL + "%)";
+
+		} if(scrollToggle === 1) {
+			
+			(lastScroll > newScroll) ?  spanL += scrollYPercent : spanL += scrollYPercent;
+			staff_textL[i].style.transform = "translateY(-50%) translateX(" + spanL + "%)";
+			
+		}
+
+	}
+	}
+};
