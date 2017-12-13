4a596c7bd529cc9fa98f9fbfeb8ace7f2afd07f4
Curtis Beall <curtisbeall@outlook.com>
2017-12-12 17:05:35 -0800

    Major Update - 12-12-17
    Fixed Homepage issue for small screens.
    Fixed scroll animation on About page.
    Fixed Image scaling on workpage.
    Removed misc browser issues.

--- js/script.js
+++ js/script.js
@@ -1,96 +1,109 @@
-// Drop Down Menu Function
-(function() {
-	const toggle = document.getElementById("c-hamburger");
-	const hide = document.getElementById("nav-bar");
+//On Orientation Change
+window.addEventListener("orientationchange", function() {
+    location.reload(true);
+});
 
-	toggle.addEventListener( "click", function(e) {
-	e.preventDefault();
-	(this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
-	(hide.classList.contains("hidden") === true) ? hide.classList.remove("hidden") : hide.classList.add("hidden");
-	});
+// Drop Down Menu Function
+(function () {
+    const toggle = document.getElementById("c-hamburger");
+    const hide = document.getElementById("nav-bar");
+
+    toggle.addEventListener("click", function (e) {
+        e.preventDefault();
+        (this.classList.contains("is-active") === true) ? this.classList.remove("is-active"): this.classList.add("is-active");
+        (hide.classList.contains("hidden") === true) ? hide.classList.remove("hidden"): hide.classList.add("hidden");
+    });
 })();
 
 //Setup About Page Before Load Function
 window.onbeforeunload = () => {
-	
-	const about = 'file:///C:/Users/Curtis/Documents/Git%20Repos/Splash/about.html';
-	
-     if (window.location.href === about) {
-		window.scrollTo(0,0);
-	 }
+
+    const about = '/C:/Users/Temp/Documents/Git%20Repos/Splash/about.html';
+    if (window.location.pathname === about) {
+        window.scrollTo(0, 0);
+    }
 };
 
 //Setup About Page Onload Function
 window.onload = () => {
-	
-	const about = 'file:///C:/Users/Curtis/Documents/Git%20Repos/Splash/about.html';
-	
-     if (window.location.href === about) {
-		window.scrollBy(0,1);
-	 }
-};
 
+    const about = '/C:/Users/Temp/Documents/Git%20Repos/Splash/about.html';
+    if (window.location.pathname === about) {
+        const staff_text = document.getElementsByClassName("staff_textbg");
+
+        for (var i = 0; i < staff_text.length; i++) {
+            if (i % 2 === 0) { // index is even
+                staff_text[i].style.transform = "translateY(-50%) translateX(" + -52 + "%)";
+            } else if ((i % 2 === 1)) {
+                staff_text[i].style.transform = "translateY(-50%) translateX(" + -104 + "%)";
+            }
+        }
+    }
+};
 
 //About Page Text Scroll Var
 
-	let scrollToggle = 1;
-	let spanlife = -52;
-	let lastScrollY = 0;
+let scrollToggle = 1;
+let currentSpan = 0;
+let lastScrollY = 0;
 
 //About Page Text Scroll Function
-window.onscroll = () => {
-	if (window.innerWidth > 626) {
-	const staff_textR = document.getElementsByClassName("staff_textbg align-r");
-	const staff_textL = document.getElementsByClassName("staff_textbg align-l");
-	
-	let newScroll = (scrollY * 100 / document.documentElement.scrollHeight);
-	let lastScroll = (lastScrollY * 100 / document.documentElement.scrollHeight);
-	let scrollYPercent = (newScroll - lastScroll)*1.25;
-	let span = spanlife;
-	let spanL = spanlife+50;
+window.onscroll = (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
 
-	for (var i = 0; i < staff_textR.length; i++) {
-		
-		if(span < -53 ) {
-			
-			(scrollToggle === 1 ) ?  scrollToggle = 0: scrollToggle = 1;
-			
-		} if(span > 2) {
-			
-			(scrollToggle === 0 ) ?  scrollToggle = 1: scrollToggle = 0;
-			
-		} if(scrollToggle === 0) {
-			
-			(lastScroll < newScroll) ?  span -= scrollYPercent : span -= scrollYPercent;
-			(lastScroll < newScroll) ?  lastScrollY = scrollY: lastScrollY = scrollY;
-			spanlife = span;	
-			staff_textR[i].style.transform = "translateY(-50%) translateX(" + span + "%)";
+    let staffPos = document.getElementById("staff").offsetTop;
+    let scrollPos = window.scrollY;
+    let scrollSpan = document.getElementById('staffMember').offsetHeight;
 
-		} if(scrollToggle === 1) {
-			
-			(lastScroll > newScroll) ?  span += scrollYPercent : span += scrollYPercent;
-			(lastScroll > newScroll) ?  lastScrollY = scrollY: lastScrollY = scrollY;
-			spanlife = span;
-			staff_textR[i].style.transform = "translateY(-50%) translateX(" + span + "%)";
-		}
+    if (window.innerWidth > 626) {
+        const staff_text = document.getElementsByClassName("staff_textbg");
 
-	}
-	
+        let newScroll = (scrollY);
+        let lastScroll = (lastScrollY);
+        let span = 0;
 
-	for (var i = 0; i < staff_textL.length; i++) {
+        if (lastScroll === 0) {
+            lastScroll = newScroll;
+        }
 
-	if(scrollToggle === 0) {
+        //        if (currentSpan > scrollSpan) {
+        //            console.log("change currentSpan = 0")
+        //            currentSpan = currentSpan - scrollSpan;
+        //        }
 
-			(lastScroll < newScroll) ?  spanL -= scrollYPercent : spanL -= scrollYPercent;
-			staff_textL[i].style.transform = "translateY(-50%) translateX(" + spanL + "%)";
+        currentSpan += (newScroll - lastScroll) * scrollToggle;
+        span = ((currentSpan * 100) / scrollSpan)*.6 - 52;
+        spanL = ((currentSpan * 100) / scrollSpan)*.6 + 2;
 
-		} if(scrollToggle === 1) {
-			
-			(lastScroll > newScroll) ?  spanL += scrollYPercent : spanL += scrollYPercent;
-			staff_textL[i].style.transform = "translateY(-50%) translateX(" + spanL + "%)";
-			
-		}
+        lastScrollY = scrollY
 
-	}
-	}
+
+        for (var i = 0; i < staff_text.length; i++) {
+            if (i % 2 === 0) { // index is even
+                if(currentSpan > scrollSpan) {
+                    span = 3;
+                    currentSpan = scrollSpan;
+                   } else if (currentSpan < 0 || span === 53) {
+                    span = -53;  
+                   currentSpan = 0;
+                  }
+                staff_text[i].style.transform = "translateY(-50%) translateX(" + span + "%)";
+            } else {
+                staff_text[i].style.transform = "translateY(-50%) translateX(" + spanL + "%)";
+            }
+        }
+
+        if (span === 3 || span === -53) {
+            if (scrollToggle === 1) {
+                scrollToggle = -1;
+                console.log(currentSpan)
+            } else {
+                scrollToggle = 1;
+//                currentSpan = 0;
+                console.log(currentSpan)
+
+            }
+        }
+    }
 };
